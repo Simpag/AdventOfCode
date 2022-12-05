9a0a890e2cb8f417171e3a011d26ae1da5bf4e27
Simpag <simon.granstrom@gmail.com>
2022-12-05 18:40:02 +0100

    day 5 complete

---
+++ Day 5/part1.py
@@ -0,0 +1,52 @@
+def importStacks(lines: list[str], stacks: list[list]):
+    # 1   2   3   4   5   6   7   8   9
+    indexMap = [1, 5, 9, 13, 17, 21, 25, 29, 33] 
+    lines.reverse() # so we read from bottom to top
+    for line in lines:
+        for i in indexMap:
+            if line[i].isalpha():
+                stacks[indexMap.index(i)].append(line[i])
+
+    return stacks
+
+
+def followInstruction(inst: str, stacks: list[list]):
+    inst = inst.split(' ') # ex: move 3 from 6 to 2
+    stacksToMove = int(inst[1])
+    moveFrom = int(inst[3])-1
+    moveTo = int(inst[5])-1
+    
+    for i in range(stacksToMove):
+        print(f'Move: {stacksToMove} from {moveFrom+1} to {moveTo+1} - StackFrom: {stacks[moveFrom]} StackTo: {stacks[moveTo]}')
+        itemToMove = stacks[moveFrom].pop()
+        stacks[moveTo].append(itemToMove)
+
+    return stacks
+
+
+def getTopCrates(stacks: list[list]):
+    ret = ''
+    for stack in stacks:
+        ret += str(stack[-1])
+    
+    return ret
+
+
+def solve(file_name: str): 
+    stacks = [[] for i in range(9)]
+
+    with open(file_name) as f:
+        lines = f.readlines()
+
+        stacks = importStacks(lines[:8], stacks)
+
+        lines = lines[10:] # remove the stacks and the blank line so we're only left with instructions
+
+        for line in lines:
+            stacks = followInstruction(line, stacks)
+
+    print(getTopCrates(stacks))
+
+
+if __name__ == "__main__":
+    solve('input_saltanat.txt')
